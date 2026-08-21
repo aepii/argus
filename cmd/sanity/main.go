8b67ff67c96734a35cfffc8a2c3b107901e2f800
aepii <simentalma@yahoo.com>
2026-08-21 16:50:16 -0500

    Sanity check main.go with OpenAI integration and context handling.

--- cmd/sanity/main.go
+++ cmd/sanity/main.go
@@ -1,42 +1,103 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
-	"math/rand/v2"
 	"os"
+	"time"
 
 	"github.com/aepii/argus"
+	"github.com/joho/godotenv"
+	"github.com/openai/openai-go/v3"
+	"github.com/openai/openai-go/v3/azure"
 )
 
-func randomEmbedding(dim int) []float32 {
-	v := make([]float32, dim)
-	for i := range v {
-		v[i] = rand.Float32()*2 - 1
-	}
-	return v
-}
-
+const azureOpenAIAPIVersion = "2023-05-15"
 const embeddingDim = 384
 
 func main() {
+	godotenv.Load()
+
 	s, err := argus.NewVectorStore(".test.db", embeddingDim)
 	if err != nil {
 		slog.Error("failed to create vector store", "error", err)
 		os.Exit(1)
 	}
 
-	s.Upsert(1, "hello!", randomEmbedding(embeddingDim))
-	s.Upsert(10, "world!", randomEmbedding(embeddingDim))
-	s.Upsert(5, "hi!", randomEmbedding(embeddingDim))
-	s.Upsert(1, "bye!", randomEmbedding(embeddingDim))
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	res, err := s.Search(randomEmbedding(embeddingDim), 3)
+	client := openai.NewClient(
+		azure.WithEndpoint(os.Getenv("AZURE_OPENAI_ENDPOINT"), azureOpenAIAPIVersion),
+		azure.WithAPIKey(os.Getenv("AZURE_OPENAI_API_KEY")),
+	)
+
+	var strings = []string{"Hello World!", "Bye World!", "The sky is blue!", "I love dogs!", "I love cats!", "I hate animals!", "I feel horrible typing that previous sentence.", "The sky is really green!"}
+
+	response, err := client.Embeddings.New(
+		ctx,
+		openai.EmbeddingNewParams{
+			Model: "text-embedding-3-small",
+			Input: openai.EmbeddingNewParamsInputUnion{
+				OfArrayOfStrings: strings,
+			},
+			Dimensions: openai.Int(embeddingDim),
+		},
+	)
 	if err != nil {
-		slog.Error("failed to search vector store", "error", err)
+		slog.Error("failed to create embedding", "error", err)
+		os.Exit(1)
+	}
+
+	for id, data := range response.Data {
+		id := int64(id)
+		rawText := strings[id]
+		emb := data.Embedding
+		emb32 := make([]float32, len(emb))
+		for i, v := range emb {
+			emb32[i] = float32(v)
+		}
+		err = s.Upsert(id, rawText, emb32)
+		if err != nil {
+			slog.Error("failed to upsert embedding", "error", err, "id", id, "rawText", rawText, "emb", emb32)
+			os.Exit(1)
+		}
+	}
+
+	c, err := s.Count()
+	if err != nil {
+		slog.Error("failed to count", "error", err)
+		os.Exit(1)
+	}
+	fmt.Printf("count: %d\n", c)
+
+	const searchString = "The sky is green!"
+	response, err = client.Embeddings.New(
+		ctx,
+		openai.EmbeddingNewParams{
+			Model: "text-embedding-3-small",
+			Input: openai.EmbeddingNewParamsInputUnion{
+				OfString: openai.String(searchString),
+			},
+			Dimensions: openai.Int(embeddingDim),
+		},
+	)
+	if err != nil {
+		slog.Error("failed to create embedding", "error", err)
+		os.Exit(1)
+	}
+
+	emb := response.Data[0].Embedding
+	emb32 := make([]float32, len(emb))
+	for i, v := range emb {
+		emb32[i] = float32(v)
+	}
+
+	res, err := s.Search(emb32, 3)
+	if err != nil {
+		slog.Error("failed to search embedding", "error", err, "emb", emb32)
 		os.Exit(1)
 	}
 	fmt.Println(res)
-
-	s.Count()
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,18 @@ go 1.26.6
 
 require (
 	github.com/asg017/sqlite-vec-go-bindings v0.1.6
+	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.50
+	github.com/openai/openai-go/v3 v3.52.0
+)
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.22.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.12.0 // indirect
+	github.com/tidwall/gjson v1.19.0 // indirect
+	github.com/tidwall/match v1.2.0 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+	golang.org/x/net v0.57.0 // indirect
+	golang.org/x/text v0.40.0 // indirect
 )
